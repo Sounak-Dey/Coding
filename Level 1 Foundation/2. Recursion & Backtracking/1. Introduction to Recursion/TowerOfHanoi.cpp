d34447762fff3126900c4bec2b0f8ca2be60629d
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-04 22:42:58 +0530

    4-13

--- Level 1 Foundation/2. Recursion & Backtracking/1. Introduction to Recursion/TowerOfHanoi.cpp
+++ Level 1 Foundation/2. Recursion & Backtracking/1. Introduction to Recursion/TowerOfHanoi.cpp
@@ -3,20 +3,15 @@ using namespace std;
 
 void toh(int n, int t1, int t2, int t3)
 {
-    if(n==2)
-    {
-        cout<<1<<"["<<t1<<" -> "<<t3<<"]"<<endl;
-        cout<<2<<"["<<t1<<" -> "<<t2<<"]"<<endl;
-        cout<<1<<"["<<t3<<" -> "<<t2<<"]"<<endl;
-    }
-    else if(n>2)
+    if(n==0)
+        return;
+    else if(n>0)
     {
         toh(n-1, t1, t3, t2);
         cout<<n<<"["<<t1<<" -> "<<t2<<"]"<<endl;
         toh(n-1, t3, t2, t1);
     }
 }
-
 
 int  main() 
 {
