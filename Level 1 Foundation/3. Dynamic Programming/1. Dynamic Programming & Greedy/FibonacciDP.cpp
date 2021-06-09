e85b1f8604510365ed2bf19c94806b4cc0da26cb
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-09 14:34:48 +0530

    9-2

---
+++ Level 1 Foundation/3. Dynamic Programming/1. Dynamic Programming & Greedy/FibonacciDP.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n, vector<int> &mem)
+{
+    if(n==0 || n==1)
+        return n;
+
+    if(mem[n] != 0)
+        return mem[n];
+
+    mem[n] = fib(n-1, mem) + fib(n-2, mem);
+
+    return mem[n];
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> mem(n+1, 0);
+
+    cout<<fib(n, mem)<<endl;
+    
+    return 0;
+}
