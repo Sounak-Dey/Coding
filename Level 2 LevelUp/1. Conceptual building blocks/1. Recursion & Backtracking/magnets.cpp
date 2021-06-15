b91429333b2e99dbd446d769db1e15e084eb791c
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-15 15:21:04 +0530

    15-1

--- Level 2 LevelUp/1. Conceptual building blocks/1. Recursion & Backtracking/magnets.cpp
+++ Level 2 LevelUp/1. Conceptual building blocks/1. Recursion & Backtracking/magnets.cpp
@@ -14,26 +14,26 @@ void print(vector<vector<char>> &ans)
     }      
 }
 
-int countInRow(vector<vector<char>> &ans, vector<int> &rowCount, char sign, int r) // can be optimised
+int countInRow(vector<vector<char>> &ans, char sign, int r) // can be optimised
 {
     int counter = 0;
-    for(auto j=0; j<ans[0].size(); j++)
-    {
+
+    for(auto j=0; j<ans[0].size(); j++)                 // by running the checkloop only till current cell and row
         if(ans[r][j] == sign)
             counter++;
-    }
+
     return counter;
 }
 
-int countInCol(vector<vector<char>> &ans, vector<int> &colCount, char sign, int c) //can be optimised
+int countInCol(vector<vector<char>> &ans, char sign, int c) //can be optimised
 {
 
     int counter = 0;
+
     for(auto i=0; i<ans.size(); i++)
-    {
         if(ans[i][c] == sign)
             counter++;
-    }
+
     return counter;
 }
 
@@ -41,26 +41,22 @@ int countInCol(vector<vector<char>> &ans, vector<int> &colCount, char sign, int 
 bool isItSafe(vector<vector<char>> &ans, vector<int> &top, vector<int> &left, vector<int> &right, vector<int> &bottom, int r, int c, char sign)
 {
     //checking if same sign is present at left, top or right cells
-    if((c-1>=0 && ans[r][c-1] == sign) || (r-1>=0 && ans[r-1][c] == sign) || (c+1 < ans[0].size() && ans[r][c+1] == sign))
+    if((ans[r][c] != 'X') || (c-1>=0 && ans[r][c-1] == sign) || (r-1>=0 && ans[r-1][c] == sign) || (r + 1 < ans.size() && ans[r + 1][c] == sign) || (c+1 < ans[0].size() && ans[r][c+1] == sign))
         return false;
-    
+
+    int rowCount = countInRow(ans, sign, r);
+    int colCount = countInCol(ans,  sign, c);
+
     // checking for row and column counts of sign
     if(sign == '+')
     {
-        int rowCount = countInRow(ans, left, sign, r);
-        int colCount = countInCol(ans, top, sign, c);
-
         if(left[r] != -1 && rowCount >= left[r])
             return false;
         if(top[c] != -1 && colCount >= top[c])
             return false;
     }
-    else
+    if(sign == '-')
     {
-
-        int rowCount = countInRow(ans, right, sign, r);
-        int colCount = countInCol(ans, bottom, sign, c);
-
         if(right[r] != -1 && rowCount >= right[r])
             return false;
         if(bottom[c] != -1 && colCount >= bottom[c])
@@ -75,28 +71,27 @@ bool isAnsValid(vector<vector<char>> &ans, vector<int> &top, vector<int> &left, 
 {
     for(int i=0; i<top.size(); i++)
     {
-        int colCount = countInCol(ans, top, '+', i);
-        if(top[i] != -1 && top[i] !=colCount)
+        if(top[i] != -1 && top[i] != countInCol(ans,  '+', i))
             return false;
     }
 
     for(int i=0; i<left.size(); i++)
     {
-        int rowCount = countInCol(ans, left, '+', i);
+        int rowCount = countInRow(ans,  '+', i);
         if(left[i] != -1 && left[i] !=rowCount)
             return false;
     }
 
     for(int i=0; i<bottom.size(); i++)
     {
-        int colCount = countInCol(ans, bottom, '-', i);
+        int colCount = countInCol(ans,  '-', i);
         if(bottom[i] != -1 && bottom[i] !=colCount)
             return false;
     }
 
     for(int i=0; i<right.size(); i++)
     {
-        int rowCount = countInCol(ans, right, '-', i);
+        int rowCount = countInRow(ans,  '-', i);
         if(right[i] != -1 && right[i] !=rowCount)
             return false;
     }
@@ -106,80 +101,84 @@ bool isAnsValid(vector<vector<char>> &ans, vector<int> &top, vector<int> &left, 
 
 bool solution(vector<vector<char>> &arr, vector<int> &top, vector<int> &left, vector<int> &right, vector<int> &bottom, vector<vector<char>> &ans, int r, int c)
 {
-    if(c == arr[0].size())
+    if(c >= arr[0].size())
     {
         c = 0;
         r++;
     }
 
-    if(r == arr.size())
+    if(r >= arr.size())
     {
         if(isAnsValid(ans, top, left, right, bottom))
             return true;
-
-        return false;
+        else
+            return false;
     }
 
-    //checking if the magnet needs to placed in horizontal allignment i.e arr[r][c] and arr[r][c+1] contains L R respectively
-    if(arr[r][c] == 'L')
+    if(ans[r][c] == 'X')
     {
-        //checking if we can place '+ -'
-        if(isItSafe(ans, top, left, right, bottom, r, c, '+') && isItSafe(ans, top, left, right, bottom, r, c+1, '-'))
+        //checking if the magnet needs to placed in horizontal allignment i.e arr[r][c] and arr[r][c+1] contains L R respectively
+        if(arr[r][c] == 'L' && arr[r][c+1] == 'R')
         {
-            //placing '+ -'
-            ans[r][c] = '+';
-            ans[r][c+1] = '-';
+            //checking if we can place '+ -'
+            if(isItSafe(ans, top, left, right, bottom, r, c, '+') && isItSafe(ans, top, left, right, bottom, r, c+1, '-'))
+            {
+                //placing '+ -'
+                ans[r][c] = '+';
+                ans[r][c+1] = '-';
 
-            if(solution(arr, top, left, right, bottom, ans, r, c+2))  // checking if we got a solution following this path
-                return true;
-                                                   
-            ans[r][c] = 'X';        //clearing while backtracking
-            ans[r][c+1] = 'X';
+                if(solution(arr, top, left, right, bottom, ans, r, c+2))  // checking if we got a solution following this path
+                    return true;
+                                                    
+                ans[r][c] = 'X';        //clearing while backtracking
+                ans[r][c+1] = 'X';
+            }
+
+            //checking if we can place '- +'
+            if(isItSafe(ans, top, left, right, bottom, r, c, '-') && isItSafe(ans, top, left, right, bottom, r, c+1, '+'))
+            {
+                ans[r][c] = '-';
+                ans[r][c+1] = '+';
+
+                if(solution(arr, top, left, right, bottom, ans, r, c+2))
+                    return true;
+                                                    
+                ans[r][c] = 'X';     
+                ans[r][c+1] = 'X';
+            }
         }
 
-        //checking if we can place '- +'
-        if(isItSafe(ans, top, left, right, bottom, r, c, '-') && isItSafe(ans, top, left, right, bottom, r, c+1, '+'))
+        //vertical allignment
+        else if(arr[r][c] == 'T' && arr[r+1][c] == 'B')
         {
-            ans[r][c] = '-';
-            ans[r][c+1] = '+';
+            if(isItSafe(ans, top, left, right, bottom, r, c, '+') && isItSafe(ans, top, left, right, bottom, r+1, c, '-'))
+            {
+                ans[r][c] = '+';
+                ans[r+1][c] = '-';
 
-            if(solution(arr, top, left, right, bottom, ans, r, c+2))
-                return true;
-                                                   
-            ans[r][c] = 'X';     
-            ans[r][c+1] = 'X';
+                if(solution(arr, top, left, right, bottom, ans, r, c+1))  
+                    return true;
+                                                    
+                ans[r][c] = 'X';        
+                ans[r+1][c] = 'X';
+            }
+
+            if(isItSafe(ans, top, left, right, bottom, r, c, '-') && isItSafe(ans, top, left, right, bottom, r+1, c, '+'))
+            {
+                ans[r][c] = '-';
+                ans[r+1][c] = '+';
+
+                if(solution(arr, top, left, right, bottom, ans, r, c+1))  
+                    return true;
+                                                    
+                ans[r][c] = 'X';        
+                ans[r+1][c] = 'X';
+            }
+
         }
+
     }
-
-    //vertical allignment
-    else if(arr[r][c] == 'T')
-    {
-        if(isItSafe(ans, top, left, right, bottom, r, c, '+') && isItSafe(ans, top, left, right, bottom, r+1, c, '-'))
-        {
-            ans[r][c] = '+';
-            ans[r][c+1] = '-';
-
-            if(solution(arr, top, left, right, bottom, ans, r, c+1))  
-                return true;
-                                                   
-            ans[r][c] = 'X';        
-            ans[r][c+1] = 'X';
-        }
-
-        if(isItSafe(ans, top, left, right, bottom, r, c, '-') && isItSafe(ans, top, left, right, bottom, r+1, c, '+'))
-        {
-            ans[r][c] = '-';
-            ans[r][c+1] = '+';
-
-            if(solution(arr, top, left, right, bottom, ans, r, c+1))  
-                return true;
-                                                   
-            ans[r][c] = 'X';        
-            ans[r][c+1] = 'X';
-        }
-
-    }
-
+    
     //no
     if(solution(arr, top, left, right, bottom, ans, r, c+1));
         return true;
