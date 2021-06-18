17c6b4ece8fd4c18d92915fa60c8384c9bdf07e1
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-18 14:48:40 +0530

    18-1

---
+++ Level 2 LevelUp/1. Conceptual building blocks/1. Recursion & Backtracking/permutation1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void permutations1(vector<int> &boxes, int ci, int r)
+{
+    //levels - r
+    //options - boxes
+
+    if(ci > r)
+    {
+        for(int i=0; i<boxes.size(); i++)
+            cout<<boxes[i];
+        cout<<endl;
+        return;
+    }
+
+    for(int i=0; i<boxes.size(); i++)
+        if(boxes[i] == 0)
+        {
+            boxes[i] = ci;
+            permutations1(boxes, ci+1, r);
+            boxes[i] = 0;
+        }
+}
+
+
+int main()
+{
+    int n, r;
+    cin>>n>>r;
+
+    vector<int> boxes(n, 0);
+
+    permutations1(boxes, 1, r);
+
+    return 0;
+
+}
