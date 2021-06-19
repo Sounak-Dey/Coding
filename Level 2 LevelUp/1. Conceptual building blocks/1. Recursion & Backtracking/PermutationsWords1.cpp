15efc9f2ace0dea5e9df2d37181ebc3104b665ae
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-19 22:31:25 +0530

    19-9

---
+++ Level 2 LevelUp/1. Conceptual building blocks/1. Recursion & Backtracking/PermutationsWords1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+
+//bi = box index
+//tb = total boxes
+void pw1(int bi, int tb, map<char, int> &freqMap, string asf)
+{
+    if(bi == tb)
+    {
+        cout<<asf<<endl;
+        return;
+    }
+
+    for( auto &i: freqMap)
+        if(freqMap[i.first] > 0)
+        {
+            freqMap[i.first]--;
+            pw1(bi+1, tb, freqMap, asf + i.first);
+            freqMap[i.first]++;
+        }
+}
+
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    map<char, int> freqMap;
+    for(int i=0; i<str.length(); i++)   //calculating frequency of each character in string
+        freqMap[str[i]]++ ; 
+
+    pw1(0, str.size(), freqMap, "");
+    return 0;
+
+}
