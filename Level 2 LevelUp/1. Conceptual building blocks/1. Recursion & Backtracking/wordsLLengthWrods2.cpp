3b5597c570aeca6d74e43b8fb3aa6d1a79b1af23
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-21 15:24:33 +0530

    21-1

---
+++ Level 2 LevelUp/1. Conceptual building blocks/1. Recursion & Backtracking/wordsLLengthWrods2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<unordered_set>
+
+using namespace std;
+
+// levels - spots
+//options - characters
+// levels < options
+// so options dont have a choiec of going blank
+void solution(string &ustr, int k, string asf)
+{
+    
+    if(k == 0)
+    {
+        cout<<asf<<endl;
+        return;
+    }
+    // if(ustr.length() == 0)  // not needed as k will reach 0 first everytime
+    //     return;
+
+    for(int i=0; i<ustr.length(); i++)
+    {
+        char ch = ustr[i];
+        ustr.erase(i, 1);
+        solution(ustr, k-1, asf + ch); // don't do k-- , always do k-1
+        ustr.insert(i, 1, ch);
+    }
+}
+
+
+int main()
+{
+    string str, ustr = "";
+    cin>>str;
+    int k;
+    cin>>k;
+
+    unordered_set<char> unique;
+    for(auto ch: str)
+        if(unique.find(ch) == unique.end())
+        {
+            unique.insert(ch);
+            ustr += ch;
+        }
+    
+    solution(ustr, k, "");
+    return 0;
+}
