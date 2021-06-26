96c72336f07e53af830cb3ebd8ed89340cf8814e
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-06-26 07:44:14 +0530

    26-2

---
+++ Level 1 Foundation/3. Dynamic Programming/1. Dynamic Programming & Greedy/TilingWithM*1Tiles.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int solve(int n, int m)
+{
+    vector<int> dp(n+1);
+    //dp[i] represents the total number of ways we can arrange
+    // the non-distinguishable tiles in the 'm * i' floor
+    
+    if(n >= m)    
+    {
+        for(int i=1; i<m; i++)  //base case
+            dp[i] = 1;             // till i<m we can place only vertically
+        
+        dp[m] = 2;      //base case, in m*m we have w ways to arrange, all horzontal or all vertical
+
+        for(int i=m+1; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-m];
+        }
+        cout<<dp[n];
+    }
+    else        //n<m we can place only vertically
+        cout<<1;    
+}
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    solve(n, m);
+    return 0;
+}
