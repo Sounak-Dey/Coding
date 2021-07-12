c7b358c8d88f733b77d0e3031015dc734078e217
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-07-12 20:06:13 +0530

    42-3

---
+++ Level 1 Foundation/5. Basic Data Structures/2. Linked Lists/3. Remove First In Linkedlist.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;     
+};
+
+class LinkedList
+{
+    public:
+
+        node *head, *tail;
+        int size;
+        
+        LinkedList()       
+        {
+            head = NULL;
+            tail = NULL;
+            size = 0;
+        }
+
+        void addLast(int val)
+        {
+            node *temp = new node();
+            
+            temp->data = val;
+            temp->next = NULL;
+            
+            if(head == NULL)
+                head = temp;
+            else
+                tail->next = temp;
+                
+            tail = temp;
+            size++;
+        }
+
+        void removeFirst()
+        {
+            if(head == NULL)
+                cout<<"List is empty"<<endl;
+                
+            else if(head == tail)
+            {
+                head = NULL;
+                tail = NULL;
+                size--;
+            }
+            else
+            {
+                head = head->next;
+                size--;    
+            }
+            
+        }
+
+        int showSize()
+        {
+            return size;
+        }
+
+        void display()
+        {
+            node *temp;
+            temp = head;
+
+            while(temp!= NULL)
+            {
+                cout<<temp->data<<" ";
+                temp = temp->next;
+            }
+            cout<<endl;
+        }
+};
+
+
+
+int main()
+{
+    LinkedList list;
+
+    string str;
+    getline(cin,str);
+
+    while(str.compare("quit") != 0)
+    {
+        if(str.find("addLast") == 0)
+        {
+            int val = stoi(str.substr(str.find(" ") + 1));
+            list.addLast(val);
+        }
+        else if(str.find("size") == 0)
+            cout<<list.showSize()<<endl;
+
+        else if(str.find("display") == 0)
+            list.display();
+            
+        else if(str.find("removeFirst") == 0)
+            list.removeFirst();
+
+        getline(cin, str);
+    }
+
+    return 0;
+}
+
