7e46b00f3e671ce7ce637d91feb2c2b3750d54e5
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-07-16 19:45:29 +0530

    46-5

---
+++ Level 1 Foundation/5. Basic Data Structures/3. Generic Tree/4. Generic Tree - Traversals (pre-order, Post-order).cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm>
+
+using namespace std;
+
+//structure of a node of the tree
+struct node
+{
+    int data;
+    vector<node *> children; 
+                             
+};
+
+
+//diplay the tree
+void display(node *nd)
+{
+    string str = to_string(nd->data) + " -> ";  
+
+    for(auto child: nd->children)       
+        str += to_string(child->data) + ", ";  
+
+    str += ".";
+    cout<<str<<endl;
+
+    for(auto child: nd->children)   
+        display(child);
+}
+
+
+//construct a tree from an array using Left to Right - Depth First Search
+node *construct(vector<int> arr)
+{
+    node *root = nullptr;
+    stack<node *> st;       
+
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(arr[i] == -1)    
+            st.pop();
+        
+        else        
+        {
+            node *t = new node();       
+            t->data = arr[i];           
+
+            if(st.empty())      
+                root = t;
+            else
+                st.top()->children.push_back(t);                         
+            
+            st.push(t);     
+        }
+    }
+
+    return root;
+}
+
+int size(node *nd)
+{
+    // if(nd == nullptr)    //not needed
+    //     return 0;        
+
+    int count = 0;
+    for(auto child: nd->children)
+        count += size(child);
+
+    return count + 1;
+}
+
+int maxv(node *nd)
+{
+    int m = nd->data;
+    for(auto child: nd->children)
+        m = max(m, maxv(child));
+
+    return m;
+}
+
+int height(node *nd)
+{
+    int h = 0;
+    for(auto child: nd->children)
+        h = max(h, height(child) + 1); // since height is calculated using edges, & edge exists only if child exists
+
+    return h;
+}
+
+void traversals(node *nd)
+{
+    cout<<"Node Pre "<<nd->data<<endl;
+
+    for(auto child: nd->children)
+    {
+        cout<<"Edge Pre "<<nd->data<<"--"<<child->data<<endl;
+        traversals(child);
+        cout<<"Edge Post "<<nd->data<<"--"<<child->data<<endl;
+    }
+
+    cout<<"Node Post "<<nd->data<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(auto &i: arr)
+        cin>>i;
+    
+    node *root = construct(arr);
+    traversals(root);
+    // display(root);
+
+    return 0;
+}
