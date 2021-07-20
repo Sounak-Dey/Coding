673d729f87f53ade73623ae4ea2eb67fa2c7d18b
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-07-20 23:12:15 +0530

    50-2

---
+++ Level 1 Foundation/5. Basic Data Structures/4. Binary Tree/03. Levelorder Traversal Of Binary Tree.cpp
@@ -0,0 +1,170 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+
+    node(int data, node *left, node *right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+
+void display(node *nd)
+{
+    if(nd == nullptr)
+        return;
+
+
+    string str = "";
+
+    str += (nd->left == nullptr) ? "." : to_string(nd->left->data);
+    str += " <- " + to_string(nd->data) + " -> ";
+    str += (nd->right == nullptr) ? "." : to_string(nd->right->data);
+    
+    cout<<str<<endl;
+
+    display(nd->left);
+    display(nd->right);
+}
+
+
+node *construct(vector<int> &arr)
+{
+    node *root = new node(arr[0], nullptr, nullptr);
+    
+    stack<pair<node *, int>> st; 
+    st.push({root, 1}); // st.top().second represents the state in which, the node represented by st.top().first is in
+    int idx = 1;
+
+    while(!st.empty())
+    {
+        if(st.top().second == 1) // state = 1 means visiting for adding left child
+        {
+            if(arr[idx] == NULL) // if left child is null
+            {
+                st.top().second++;  // st.top().first->left = nullptr; not needed as when we had created this node previously, we already set its both children as nullptr by default
+            }
+            else    // if left child exists
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->left = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else if(st.top().second == 2)   // state = 2 means visiting for ading right child
+        {
+            if(arr[idx] == NULL)
+            {
+               st.top().second++;
+            }
+            else
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->right = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else    // state = 3 means both children have been added, pop the stack top node
+        {
+            st.pop();
+        }
+    }
+    
+    return root;
+}
+
+int size(node *nd)
+{
+    if(nd == nullptr)
+        return 0; // identity
+
+    return size(nd->left) + size(nd->right) + 1;
+}
+
+int sum(node *nd)
+{
+    if(nd == nullptr)
+        return 0; // identity
+
+    return sum(nd->left) + sum(nd->right) + nd->data;
+}
+
+int maxv(node *nd)
+{
+    if(nd == nullptr)
+        return INT32_MIN; // identity
+
+    return  max({maxv(nd->left), maxv(nd->right), nd->data});
+}
+
+int height(node *nd)
+{
+    if(nd == nullptr)
+        return -1; // identity
+
+    int h = max(height(nd->left), height(nd->right));
+
+    return h+1;
+}
+
+void levelOrder(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        int n = q.size(); // store size of the nodes present in current level
+
+        for(int i=0; i<n; i++) // print all the nodes of current level and keep storing their children
+        {
+            if(q.front() != nullptr)
+            {
+                cout<< q.front()->data<<" ";
+
+                q.push(q.front()->left);
+                q.push(q.front()->right);
+            }
+
+            q.pop();
+        }
+
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    string c;
+    for(auto &x: arr)
+    {
+        cin>>c;
+        if(isdigit(c[0]))
+            x = stoi(c);
+        else
+            x = NULL;
+    }
+
+    node *root = construct(arr);
+    levelOrder(root);
+    
+    return 0;
+}
