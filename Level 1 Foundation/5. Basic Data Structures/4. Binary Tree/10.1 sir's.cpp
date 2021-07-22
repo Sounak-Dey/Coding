572b5bf15d9599bce2ec307860999d43034b4eda
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-07-22 06:17:08 +0530

    52-2

---
+++ Level 1 Foundation/5. Basic Data Structures/4. Binary Tree/10.1 sir's.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+
+    node(int data, node *left, node *right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+void display(node *nd)
+{
+    if(nd == nullptr)
+        return;
+
+
+    string str = "";
+
+    str += (nd->left == nullptr) ? "." : to_string(nd->left->data);
+    str += " <- " + to_string(nd->data) + " -> ";
+    str += (nd->right == nullptr) ? "." : to_string(nd->right->data);
+    
+    cout<<str<<endl;
+
+    display(nd->left);
+    display(nd->right);
+}
+
+node *construct(vector<int> &arr)
+{
+    node *root = new node(arr[0], nullptr, nullptr);
+    
+    stack<pair<node *, int>> st; 
+    st.push({root, 1}); // st.top().second represents the state in which, the node represented by st.top().first is in
+    int idx = 1;
+
+    while(!st.empty())
+    {
+        if(st.top().second == 1) // state = 1 means visiting for adding left child
+        {
+            if(arr[idx] == NULL) // if left child is null
+            {
+                st.top().second++;  // st.top().first->left = nullptr; not needed as when we had created this node previously, we already set its both children as nullptr by default
+            }
+            else    // if left child exists
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->left = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else if(st.top().second == 2)   // state = 2 means visiting for ading right child
+        {
+            if(arr[idx] == NULL)
+            {
+               st.top().second++;
+            }
+            else
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->right = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else    // state = 3 means both children have been added, pop the stack top node
+        {
+            st.pop();
+        }
+    }
+    
+    return root;
+}
+
+void display(vector<int>& arr)
+{
+    cout << "[";
+    for(int i=0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+        if(i < arr.size() -1) 
+            cout << ", ";
+    }
+    
+    cout << "]"<<endl;
+}
+
+node *transBackFromLeftClonedTree(node *nd) //using post order
+{
+    if(nd == nullptr)
+        return nullptr;
+
+    node *lnn = transBackFromLeftClonedTree(nd->left->left);
+    transBackFromLeftClonedTree(nd->right);
+
+    nd->left = lnn; // removing clones while returning back from recursion
+
+    return nd;
+}
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    string c;
+    for(auto &x: arr)
+    {
+        cin>>c;
+        if(isdigit(c[0]))
+            x = stoi(c);
+        else
+            x = NULL;
+    }
+
+    int lo, hi;
+    cin>>lo>>hi;
+
+    node *root = construct(arr);
+    root = transBackFromLeftClonedTree(root);
+    display(root);
+    
+    return 0;
+}
