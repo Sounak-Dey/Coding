9d065e2c37c548c1541b9a705b60aa39c6b41f21
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-07-23 21:19:28 +0530

    53-4

---
+++ Level 1 Foundation/5. Basic Data Structures/5. Binary Search Tree/02. Size, Sum, Max, Min, Find In Bst.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+
+    node(int data, node *left, node *right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+
+void display(node *nd)
+{
+    if(nd == nullptr)
+        return;
+
+
+    string str = "";
+
+    str += (nd->left == nullptr) ? "." : to_string(nd->left->data);
+    str += " <- " + to_string(nd->data) + " -> ";
+    str += (nd->right == nullptr) ? "." : to_string(nd->right->data);
+    
+    cout<<str<<endl;
+
+    display(nd->left);
+    display(nd->right);
+}
+
+node *construct(vector<int> &arr)
+{
+    node *root = new node(arr[0], nullptr, nullptr);
+    
+    stack<pair<node *, int>> st; 
+    st.push({root, 1}); // st.top().second represents the state in which, the node represented by st.top().first is in
+    int idx = 1;
+
+    while(!st.empty())
+    {
+        if(st.top().second == 1) // state = 1 means visiting for adding left child
+        {
+            if(arr[idx] == NULL) // if left child is null
+            {
+                st.top().second++;  // st.top().first->left = nullptr; not needed as when we had created this node previously, we already set its both children as nullptr by default
+            }
+            else    // if left child exists
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->left = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else if(st.top().second == 2)   // state = 2 means visiting for ading right child
+        {
+            if(arr[idx] == NULL)
+            {
+               st.top().second++;
+            }
+            else
+            {
+                node *n = new node(arr[idx], nullptr, nullptr);
+                st.top().first->right = n;
+                st.top().second++;
+                st.push({n, 1});
+            }
+            idx++;
+        }
+        else    // state = 3 means both children have been added, pop the stack top node
+        {
+            st.pop();
+        }
+    }
+    
+    return root;
+}
+
+int size(node *nd)
+{
+    if(nd == nullptr)
+        return 0;
+
+    return size(nd->left) + size(nd->right) + 1;
+}
+
+int sum(node *nd)
+{
+    if(nd == nullptr)
+        return 0;
+
+    return sum(nd->left) + sum(nd->right) + nd->data;
+}
+
+int maxv(node *nd)
+{
+    if(nd->right == nullptr)
+        return nd->data;
+
+    return maxv(nd->right);
+}
+
+int minv(node *nd)
+{
+    if(nd->left == nullptr)
+        return nd->data;
+
+    return minv(nd->left);
+}
+
+int height(node *nd)
+{
+    if(nd == nullptr)
+        return -1; // identity
+
+    int h = max(height(nd->left), height(nd->right));
+
+    return h+1;
+}
+
+bool findv(node *nd, int data)
+{
+    if(nd == nullptr)
+        return false;
+
+    if(nd->data == data)
+        return true;
+
+    if(data > nd->data)
+    {
+        if(findv(nd->right, data))
+            return true;
+    }
+    else
+    {
+        if(findv(nd->left, data))
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    string c;
+    for(auto &x: arr)
+    {
+        cin>>c;
+        if(isdigit(c[0]))
+            x = stoi(c);
+        else
+            x = NULL;
+    }
+
+    int data;
+    cin>>data;
+
+    node *root = construct(arr);
+    cout<<size(root)<<endl;
+    cout<<sum(root)<<endl;
+    cout<<maxv(root)<<endl;
+    cout<<minv(root)<<endl;
+    
+    if(findv(root, data))
+        cout<<"true";
+    else 
+        cout<<"false";
+    
+    return 0;
+}
