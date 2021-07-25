7151aa23620b979a2003a6bee77efc4273a9b6e7
Sounak Dey <sounak.dey@iiitb.org>
2021-07-25 22:30:37 +0530

    55-4

---
+++ Level 1 Foundation/6. Advanced Data Structures/1. HashMap and Heaps/04.1 Correct.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void lcse(vector<int> &a)
+{
+    unordered_map<int, bool> boolMap;
+
+    for(auto i: a)
+        boolMap[i] = true;
+    
+    for(auto i: a)
+    {
+        if(boolMap.find(i-1) != boolMap.end())
+            boolMap[i] = false;
+    }
+
+    int msp = 0;
+    int ml = 0;
+    for(auto i: a) // if conflicts occur we need to print the elements occuring first in array
+    {
+        if(boolMap[i] == true)
+        {
+            int tl = 1;
+            int tsp = i;
+
+            while(boolMap.find(tsp + tl) != boolMap.end())
+                tl++;
+            
+            if(tl > ml)
+            {
+                msp = tsp;
+                ml = tl;
+            }
+        }
+    }
+
+    for(int i=0; i<ml; i++)
+        cout<<msp+i<<endl;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    for(auto &x: a)
+        cin>>x;
+    
+    lcse(a);
+
+    return 0;
+}
